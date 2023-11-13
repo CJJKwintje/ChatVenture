7d558e6fc66ca15892905f32f7409b70bbceb24f
CJJKwintje <christiaan.kwint@icloud.com>
2023-11-13 15:07:50 +0100

    Update fetchSheetData.js

--- netlify/functions/fetchSheetData.js
+++ netlify/functions/fetchSheetData.js
@@ -4,7 +4,6 @@ const { GoogleSpreadsheet } = require('google-spreadsheet');
 
 exports.handler = async (event, context) => {
   console.log("Function start: Fetching data based on country");
-
   const country = event.queryStringParameters.country;
   console.log("Received country parameter:", country);
 
@@ -12,27 +11,24 @@ exports.handler = async (event, context) => {
     console.log("Initializing GoogleSpreadsheet");
     const doc = new GoogleSpreadsheet('1S6yIEUt4vTbb66-gcUXKJB9V1OjhCrLvU6HSPFj_FYg');
 
-    console.log("Parsing credentials");
-    const creds = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
-    console.log("Parsed credentials:", creds);
-
-    console.log("Attempting to authenticate with useServiceAccountAuth");
-    await doc.useServiceAccountAuth(creds);
+    console.log("Attempting to authenticate");
+    await doc.useServiceAccountAuth({
+      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    });
     console.log("Authenticated successfully");
 
     console.log("Loading document info");
     await doc.loadInfo();
     console.log("Document info loaded");
 
-    console.log("Accessing sheet by title: 'Reisaanbod'");
-    const sheet = doc.sheetsByTitle['Reisaanbod']; // Gebruik de exacte naam van het tabblad
+    const sheet = doc.sheetsByTitle['Reisaanbod'];
     console.log("Sheet accessed:", sheet.title);
 
     console.log("Fetching rows");
     const rows = await sheet.getRows();
     console.log(`Fetched ${rows.length} rows`);
 
-    console.log(`Filtering rows based on country: ${country}`);
     const filteredRows = rows.filter(row => row._rawData[0] === country);
     console.log(`Filtered rows count: ${filteredRows.length}`);
 
@@ -45,6 +41,3 @@ exports.handler = async (event, context) => {
     return { statusCode: 500, body: error.toString() };
   }
 };
-
-
-
