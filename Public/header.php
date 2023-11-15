4505fa55fd4b28641b0a1690252c374207f2a4a1
CJJKwintje <christiaan.kwint@icloud.com>
2023-11-15 21:11:45 +0100

    release

---
+++ Public/header.php
@@ -0,0 +1,10 @@
+<header>
+  <div class="headerleft">
+    <div class="logo">
+      <a href="/index.html">
+        <img src="assets/logo.jpg" alt="Chatventure Logo" class="logoimage">
+        Chatventure
+      </a>
+    </div>
+  </div>
+</header>
